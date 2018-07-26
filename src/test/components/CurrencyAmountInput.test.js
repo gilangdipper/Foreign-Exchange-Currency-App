dd71cb81aab60f0fdf1d75f6b6cdf5d64be4ffec
Gilang Al Khansa <gilang.alkhansa@iproperty.com>
2018-07-26 14:12:00 +0700

    Update unit test for CurrencyOption && CurrencyAmountInput

--- src/test/components/CurrencyAmountInput.test.js
+++ src/test/components/CurrencyAmountInput.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CurrencyAmountInput from '../../component/CurrencyAmountInput';
 import { Input } from 'antd';
 
-describe('<CurrenciesCard />', () => {
+describe('<CurrencyAmountInput />', () => {
   const baseProps = {
     amount: 1,
 		removeCurrency: _ => _
@@ -11,8 +11,8 @@ describe('<CurrenciesCard />', () => {
 	const ComponentBase = props =>
     shallow(<CurrencyAmountInput {...baseProps} {...props} />);
 
-  it('Should be render Currency card if there ara Currency data', () => {
-    expect(ComponentBase().find(Input).prop('value')).toBe(0);
+  it('Should set amout value from props', () => {
+    expect(ComponentBase().find(Input).prop('value')).toBe(1);
   });
 });
 2
--- src/test/components/CurrencyOption.test.js
+++ src/test/components/CurrencyOption.test.js
@@ -3,7 +3,7 @@ import CurrencyOption from '../../component/CurrencyOption';
 import { OptionWrapper } from '../../component/CurrencyOption';
 import { Button } from 'antd';
 
-describe('<CurrenciesCard />', () => {
+describe('<CurrencyOption />', () => {
   const baseProps = {
     amount: 1,
     currency: [
@@ -21,7 +21,11 @@ describe('<CurrenciesCard />', () => {
 		shallow(<CurrencyOption {...baseProps} {...props} />);
 		
 
-  it('Should be render Currency card if there ara Currency data', () => {
+  it('Should be render button "Add More Curencie"', () => {
     expect(ComponentBase().find(OptionWrapper).find(Button).prop('children')).toBe('Add More Curencies');
+	});
+
+	it('Should be render button submit the Currency if shouldShowFrom state is true', () => {
+    expect(ComponentBase().setState({ shouldShowFrom: true }).find(OptionWrapper).find(Button).prop('children')).toBe('Submit');
 	});
 });
